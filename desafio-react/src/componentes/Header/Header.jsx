882b0cb571b2d9915dd1ef90e3d1e60045993169
VitoriaSampaiodev <vitoriadevsampaio@gmail.com>
2024-02-26 23:57:02 -0300

    concluído

---
+++ desafio-react/src/componentes/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css"
+
+
+
+function Header(){
+
+    return(
+        <>
+        <div>
+        <h1>Olá, pessoal</h1>
+        <img src="https://imgb.ifunny.co/images/728674aadb15d071bf8a1990245bda5f6e2193b1bba39777251fedc08d7566bc_1.jpg" alt="gatinho" />
+        </div>
+        </>
+        )
+}
+
+export default Header
